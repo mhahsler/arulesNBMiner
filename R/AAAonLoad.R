c5847954339c901892e6b53a9b22ef03c3a9aa07
mhahsler <michael@hahsler.net>
2018-01-07 13:13:13 -0600

    Added Java version check.

--- R/AAAonLoad.R
+++ R/AAAonLoad.R
@@ -1,4 +1,13 @@
-.onLoad <-
-function(libname, pkgname)
-    rJava::.jpackage(pkgname, lib.loc = libname)
+## for rJava
 
+.onLoad <- function(libname, pkgname) {
+  options(java.parameters="-Xrs")  ### so sun java does not kill R on CTRL-C
+  .jpackage(pkgname, lib.loc = libname)
+}
+
+.jinit()
+jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+if(substr(jv, 1L, 1L) == "1") {
+  jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+  if(jvn < 1.5) stop("Java 5 or higher is needed for this package but not available")
+}
